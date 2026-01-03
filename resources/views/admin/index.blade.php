a9e8eac5a41234f15087c241f5f00d377a1ab298
Nate <8bitsodagamer@gmail.com>
2026-01-03 22:36:05 +0400

    admin panel created

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin panel</title>
-</head>
-<body>
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admin dashboard') }}
-        </h2>
-    </x-slot>
-</x-app-layout>
+<html>
+  <head> 
+ @include('admin.css')
 
-</body>
+  </head>
+  <body>
+    @include('admin.header')
+
+    <div class="d-flex align-items-stretch">
+        @include('admin.sidebar')
+        
+        @include('admin.main')
+
+        @include('admin.footer')
+    </div>
+    
+    @include('admin.script')
+  </body>
 </html>
