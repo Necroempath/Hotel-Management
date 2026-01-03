518fbdc7749b4764d47d3830fb8aeb71f7df48de
Nate <8bitsodagamer@gmail.com>
2026-01-03 19:19:23 +0400

    auth completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+route::get('/home', [AdminController::class, 'index'])->name('home');
